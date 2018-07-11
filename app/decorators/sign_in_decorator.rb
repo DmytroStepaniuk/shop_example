0f0dd543eb8611b19dcece64877df9281bf70e66
Ivan <khyzhniak.i@outlook.com>
2018-07-11 08:56:32 +0000

    Fix decorator

--- app/decorators/sign_in_decorator.rb
+++ app/decorators/sign_in_decorator.rb
@@ -7,7 +7,7 @@ class SignInDecorator < Draper::Decorator
     {
       user:       {
                     email: user.email,
-                    full_name: user.full_name,
+                    full_name: user.decorate.full_name,
                     pending_orders_count: user.orders.where(status: :pending).count
                   },
       auth_token: session.auth_token
