7498743a6d9be628f6715aa70895825cdf219645
Ivan Khyzhniak <khyzhniak.i@outlook.com>
2018-07-16 10:02:41 +0000

    fix validate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
+  
+  skip_before_action :verify_authenticity_token, if: -> { request.format.json? }
   
   helper_method :resource, :current_user, :current_session
 
