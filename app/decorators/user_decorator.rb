e56c094a08e7a818dc1269b175b07195127206f5
IX <khyzhniak.i@outlook.com>
2018-07-19 22:46:34 +0300

    Update user_decorator.rb

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,14 +1,15 @@
 class UserDecorator < Draper::Decorator
   delegate_all
-
+  decorates_association :user
+  
   def full_name
     [object.first_name, object.last_name].join(' ')
   end
   
   def as_json
     {
-      email: object.email,
-      full_name: object.full_name
+      email: email,
+      full_name: full_name
     }
   end
   
