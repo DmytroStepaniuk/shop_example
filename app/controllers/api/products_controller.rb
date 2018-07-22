11d53c8bfc7d28eadc50566056f05235c5eeb28e
Ivan Khyzhniak <khyzhniak.i@outlook.com>
2018-07-22 11:49:14 +0000

    tiny refactor

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,9 +1,6 @@
 class Api::ProductsController < ApplicationController
-  def index
-  end
-  
   private
   def collection
-    @products ||= Product.all.page(params[:page]).per(10) 
+    @products ||= Product.all.page(params[:page]).per(9) 
   end 
 end
