28ad516c5ac651aacaf9a2921cc2e91012fec775
Ivan Khyzhniak <khyzhniak.i@outlook.com>
2018-07-22 08:52:09 +0000

    Add status to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ApplicationRecord
 
   belongs_to :user
   has_many :line_items
-  has_many :products, :through => :line_items
+  has_many :products, through: :line_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_134626) do
+ActiveRecord::Schema.define(version: 2018_07_22_084058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_07_19_134626) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
