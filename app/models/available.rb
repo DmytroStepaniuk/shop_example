a4f6beda43c113645b053650d476a617ed351bd2
Ivan Khyzhniak <khyzhniak.i@outlook.com>
2018-08-09 19:50:58 +0000

    add validaion

--- app/models/available.rb
+++ app/models/available.rb
@@ -1,4 +1,6 @@
 class Available < ApplicationRecord
   belongs_to :store
   belongs_to :product
+    
+  validates :quantity, numericality: { greater_than: 0 }
 end
--- spec/models/available_spec.rb
+++ spec/models/available_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Available, type: :model do
   it { should belong_to :store }
   it { should belong_to :product }
+  it { should validate_numericality_of(:quantity).is_greater_than(0) }
 end
