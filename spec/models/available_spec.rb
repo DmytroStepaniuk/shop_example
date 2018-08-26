1455082b0a7f88a0ab9080177c2a77b4b3b846b4
Ivan Khyzhniak <khyzhniak.i@outlook.com>
2018-08-26 20:02:31 +0000

    Add uniqueness_of_product_id for available

--- spec/models/available_spec.rb
+++ spec/models/available_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Available, type: :model do
   it { should belong_to :store }
   it { should belong_to :product }
   it { should validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
+
+  describe "uniqueness_of_product_id" do
+    let(:store) { Store.create }
+    let(:product) { Product.create }
+    before { store.availables.create! product: product, quantity: 1 }
+
+    it { expect { store.availables.create! product: product, quantity: 1 }.to raise_error ActiveRecord::RecordInvalid }
+  end
 end
